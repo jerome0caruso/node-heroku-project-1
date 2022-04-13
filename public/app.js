f2542d37795513ed9c3cbc57e1e5fb88689c0199
jerome0caruso <jerome0caruso@gmail.com>
2022-04-13 11:10:46 -0500

    Starting graphQL

--- public/app.js
+++ public/app.js
@@ -2,20 +2,41 @@ const input = document.getElementById('name');
 const returnData = document.getElementById('return');
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', async() => {
-    const data = await input.value;
-    await fetch('http://localhost:5000/',{
+// btn.addEventListener('click', async() => {
+//     const data = await input.value;
+//     await fetch('http://localhost:5000/',{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({data: data})
+//     })
+//         .then(res => res.json())
+//         .then(resData => {
+//             console.log(resData)
+//              returnData.innerText = resData.rData;
+//              input.value = "";
+//         })
+// })
+btn.addEventListener('click', getData);
+
+async function getData() {
+
+    const inputData = await input.value;
+    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({data: data})
-    })
-        .then(res => res.json())
-        .then(resData => {
-            console.log(resData)
-             returnData.innerText = resData.rData;
-             input.value = "";
+        body: JSON.stringify({
+            query:`
+            query {
+                greeting
+            }
+            `
         })
-})
-
+    })
+    const {data} = await response.json();
+    returnData.innerText = data.greeting;
+    input.value = "";
+}
--- server.js
+++ server.js
@@ -1,23 +1,43 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
-const TEST = process.env.TEST;
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+ const PORT = process.env.PORT || 5000;
+ const TEST = process.env.TEST;
 const dotenv = require('dotenv');
 dotenv.config();
 
-const jsonParser = bodyParser.json();
-app.use(cors());
-app.use(express.static("public"));
+// const jsonParser = bodyParser.json();
+// app.use(cors());
+// app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.status(200).send("The server is up and running")
-)
+// app.get('/', (req, res) =>
+//     res.status(200).send("The server is up and running")
+// )
 
-app.post('/', jsonParser, (req, res) => {
-    const rData = req.body.data + ' Welcome'
-    res.status(200).json({rData})
-})
+// app.post('/', jsonParser, (req, res) => {
+//     const rData = req.body.data + ' Welcome'
+//     res.status(200).json({rData})
+// })
+// app.listen(PORT);
+//---------------------GraphQL---------------------
 
-app.listen(PORT);
+const { gql, ApolloServer } = require('apollo-server');
+//Schema
+const typeDefs = gql`
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        greeting: () => {
+            return 'GraphQL World!'
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen({port: PORT})
+    .then(({url}) => console.log('Server running at' + url));
