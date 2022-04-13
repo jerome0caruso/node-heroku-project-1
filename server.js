c933a4662d7e19557119f656e79e26f5da46a27d
jerome0caruso <jerome0caruso@gmail.com>
2022-04-13 11:12:59 -0500

    Adding FE

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
- const PORT = process.env.PORT || 5000;
- const TEST = process.env.TEST;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const TEST = process.env.TEST;
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const jsonParser = bodyParser.json();
-// app.use(cors());
-// app.use(express.static("public"));
+const jsonParser = bodyParser.json();
+app.use(cors());
+app.use(express.static("public"));
 
 // app.get('/', (req, res) =>
 //     res.status(200).send("The server is up and running")
